69793430383e30ca4516f5698d240d3dc2b51e34
zers <107350306+x3zers@users.noreply.github.com>
2023-02-02 19:44:07 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# s0rgubotaltyapi
+nasil calistiririm?
+
+npm i
+
+node atahan.js
+
+node 16 ve ustu :)
+
+
+
+
+link olarak kendi apilerinizi koyun confige
---
+++ ayarlar.js
@@ -0,0 +1,51 @@
+module.exports = {
+
+    token: "MTA1NjY2OTQxMjM1MjA2NTYzOA.Golj3q.0miTfUsH46oex9jlPJ8jxBijrJki7qUClX5aXc",
+    prefix: "!",
+    sahip: "643460587405312020",
+    durum: "Minecraft Prosu Zers :);)",
+
+    api: {
+        TCKN: "",
+        TC_GSM: "",
+        GSM_TC: "",
+        AD_SOYAD: "",
+        AILEA: "",
+        AILEB: "",
+        DDOS: "",
+        SECMEN_TCKN: "",
+        SECMEN_AD_SOYAD: "",
+        AOL: "",
+        ASI: "",
+        AD: "",
+        SOYAD: "",
+        IKAMETGAH: ""
+    },
+
+    emojiler: {
+        UPLOAD: ""
+    },
+
+    roller: {
+        admin: "",
+        vip: "",
+        premium: "",
+        booster: "",
+        limitlirol: "",
+        freemium: "",
+        yardimci: ""
+    },
+
+    log: "",
+    log2: "",
+
+    wl: [""], 
+
+    sunucuID: "",
+    kanal: [""],
+    seskanal: "1063094073063383050",
+
+    sorgulimit: "5",
+
+    bakim: ""
+}
